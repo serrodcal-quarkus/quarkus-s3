888d43f7e3fcf9736bdc046840884b2591f79448
Sergio Rodríguez Calvo <sergiorodriguezcalvo@gmail.com>
2021-03-11 14:59:33 +0100

    docker-compose and set env for s3 endpoint

---
+++ aws/buckets.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -x
+#awslocal s3 mb s3://bucket
+#aws configure --profile localstack
+aws --profile localstack configure set aws_access_key_id "test-key"
+aws --profile localstack configure set aws_secret_access_key "test-secret"
+aws --profile localstack configure set aws_region "us-east-1"
+aws s3 mb s3://quarkus.s3.quickstart --profile localstack --endpoint-url=http://0.0.0.0:4566
+set +x
